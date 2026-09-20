fc590f7d46136076efc74804666aaabc7ef4197b
StarFire&MWLord <jackeggleston26@gmail.com>
2026-09-20 17:04:54 -0400

    Delete public directory

--- public/sw.js
+++
@@ -1,5 +0,0 @@
-importScripts("/uv/uv.bundle.js", "/uv.config.js", "/uv/uv.sw.js");
-const sw = new UVServiceWorker();
-self.addEventListener("fetch", event => {
-  if (sw.route(event)) event.respondWith(sw.fetch(event));
-});
