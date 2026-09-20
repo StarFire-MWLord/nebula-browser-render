fead46b9719ee12cb5ff53298c7fd8a856b1d02b
StarFire&MWLord <jackeggleston26@gmail.com>
2026-09-20 11:45:24 -0400

    Refactor fetch event listener for service worker

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,9 @@
 importScripts("/uv/uv.bundle.js", "/uv.config.js", "/uv/uv.sw.js");
+
 const sw = new UVServiceWorker();
-self.addEventListener("fetch", event => event.respondWith(sw.fetch(event)));
+
+self.addEventListener("fetch", (event) => {
+  if (sw.route(event)) {
+    event.respondWith(sw.fetch(event));
+  }
+});
